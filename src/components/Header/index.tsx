54abd806a94eb840080e9e507a618c642a6d4275
Fellipe Rezende <rezendefellipe9@gmail.com>
2021-06-10 09:27:39 -0300

    :lipstick: Starting header styles

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,27 @@
+import {
+  HomeIcon,
+  LightningBoltIcon,
+  BadgeCheckIcon,
+  CollectionIcon,
+  SearchIcon,
+  UserIcon,
+} from '@heroicons/react/outline';
+
+import { HeaderItem } from './HeaderItem';
+
 export function Header() {
-  return <h1>Header</h1>;
+  return (
+    <header className="">
+      <div className="flex flex-grow justify-evenly max-w-2xl">
+        <HeaderItem title="HOME" Icon={HomeIcon} />
+        <HeaderItem title="TRENDING" Icon={LightningBoltIcon} />
+        <HeaderItem title="VERIFIED" Icon={BadgeCheckIcon} />
+        <HeaderItem title="COLLECTIONS" Icon={CollectionIcon} />
+        <HeaderItem title="SEARCH" Icon={SearchIcon} />
+        <HeaderItem title="ACCOUNT" Icon={UserIcon} />
+      </div>
+
+      <img className="object-contain" src="/images/logo.svg" alt="voo.la" />
+    </header>
+  );
 }
