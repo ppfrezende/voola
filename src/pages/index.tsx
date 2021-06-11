1cfa453eb0357e1317f9ce9bf8f4d18a2e7b4e55
Fellipe Rezende <rezendefellipe9@gmail.com>
2021-06-11 19:34:12 -0300

    :bug: Fix error 'string as an index type'

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -42,7 +42,7 @@ export const getServerSideProps: GetServerSideProps = async context => {
   const genre = context.query.genre;
 
   const { data } = await api.get(
-    `${request[genre]?.url || request.fetchTrending.url}`
+    `${request[genre.toString()]?.url || request.fetchTrending.url}`
   );
 
   const results = data.results.map(movie => {
